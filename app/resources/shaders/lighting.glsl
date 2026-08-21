0a0f53a44df7f685829c24aa25a80ca9f0131b73
matkoccino6 <m.ivanovic.mi28@gmail.com>
2026-08-21 06:00:58 +0200

    Introduced a PBR shader model, fixed a critical bug with texture samplers not being initialized properly

--- app/resources/shaders/lighting.glsl
+++ app/resources/shaders/lighting.glsl
@@ -45,6 +45,19 @@ void main() {
 #version 330
 #define NUM_LIGHTS 3
 out vec4 FragColor;
+
+in vec2 TexCoords;
+in vec3 Normal;
+in vec3 FragPos;
+in vec3 TangentLightPos[NUM_LIGHTS - 1];
+in vec3 TangentViewPos;
+in vec3 TangentFragPos;
+
+uniform sampler2D texture_diffuse1;
+uniform sampler2D texture_normal1;
+uniform sampler2D texture_metallic_roughness1;
+uniform sampler2D texture_emissive1;
+
 struct DirLight {
     vec3 direction;
     vec3 color;
@@ -57,66 +70,113 @@ struct PointLight {
     float linear;
     float quadratic;
 };
-in vec2 TexCoords;
-in vec3 Normal;
-in vec3 FragPos;
-in vec3 TangentLightPos[NUM_LIGHTS - 1];
-in vec3 TangentViewPos;
-in vec3 TangentFragPos;
-
-uniform sampler2D uDiffuseTexture;
-uniform sampler2D uSpecularTexture;
-uniform sampler2D uNormalTexture;
-uniform sampler2D uEmissiveTexture;
-
 uniform DirLight uDirLight;
 uniform PointLight uPointLights[NUM_LIGHTS - 1];
-//uniform vec3 uLightPos;
-//uniform vec3 uViewPos;
-//uniform vec3 uLightColor;
-//uniform float uShininess = 32.0f;
+
+const float PI = 3.14159265359;
 uniform float uAmbientStrength = 0.1f;
-vec3 CalcPointLight(PointLight light, vec3 tangentLightPos, vec3 normal, vec3 fragPosTangent, vec3 viewDir, vec3 diffuseColor) {
+float DistributionGGX(vec3 N, vec3 H, float roughness) {
+    float a = roughness * roughness;
+    float a2 = a * a;
+    float NdotH = max(dot(N, H), 0.0);
+    float NdotH2 = NdotH * NdotH;
+
+    float nom = a2;
+    float denom = (NdotH2 * (a2 - 1.0) + 1.0);
+    denom = PI * denom * denom;
+
+    return nom / denom;
+}
+
+float GeometrySchlickGGX(float NdotV, float roughness) {
+    float r = (roughness + 1.0);
+    float k = (r * r) / 8.0;
+
+    float nom = NdotV;
+    float denom = NdotV * (1.0 - k) + k;
+
+    return nom / denom;
+}
+
+float GeometrySmith(vec3 N, vec3 V, vec3 L, float roughness) {
+    float NdotV = max(dot(N, V), 0.0);
+    float NdotL = max(dot(N, L), 0.0);
+    float ggx2 = GeometrySchlickGGX(NdotV, roughness);
+    float ggx1 = GeometrySchlickGGX(NdotL, roughness);
+
+    return ggx1 * ggx2;
+}
+
+vec3 fresnelSchlick(float cosTheta, vec3 F0) {
+    return F0 + (1.0 - F0) * pow(clamp(1.0 - cosTheta, 0.0, 1.0), 5.0);
+}
+vec3 CalcPointLight(PointLight light, vec3 tangentLightPos, vec3 fragPosTangent, vec3 normal, vec3 viewDir, vec3 albedo, float metallic, float roughness, vec3 F0) {
     vec3 lightDir = normalize(tangentLightPos - fragPosTangent);
+    vec3 halfVec = normalize(viewDir + lightDir);
+
     float distance = length(tangentLightPos - fragPosTangent);
     float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));
-    float diff = max(dot(normal, lightDir), 0.0);
-    vec3 diffuse = light.color * light.intensity * diff * diffuseColor * attenuation;
-    return diffuse;
+
+    vec3 radiance = light.color * light.intensity * attenuation;
+
+    float NDF = DistributionGGX(normal, halfVec, roughness);
+    float G = GeometrySmith(normal, viewDir, lightDir, roughness);
+    vec3 F = fresnelSchlick(max(dot(halfVec, viewDir), 0.0), F0);
+
+    vec3 kS = F;
+    vec3 kD = vec3(1.0) - kS;
+    kD *= 1.0 - metallic;
+
+    vec3 numerator = NDF * G * F;
+    float denominator = 4.0 * max(dot(normal, viewDir), 0.0) * max(dot(normal, lightDir), 0.0) + 0.0001;
+    vec3 specular = numerator / denominator;
+    float NdotL = max(dot(normal, lightDir), 0.0);
+
+    return (kD * albedo / PI + specular) * radiance * NdotL;
 }
-vec3 CalcDirLight(DirLight light, vec3 normal, vec3 viewDir, vec3 diffuseColor) {
+vec3 CalcDirLight(DirLight light, vec3 normal, vec3 viewDir, vec3 albedo, float metallic, float roughness, vec3 F0) {
     vec3 lightDir = normalize(-light.direction);
-    float diff = max(dot(normal, lightDir), 0.0);
-    vec3 diffuse = light.color * light.intensity * diff * diffuseColor;
-    return diffuse;
+    vec3 halfVec = normalize(viewDir + lightDir);
+    vec3 radiance = light.color * light.intensity;
+
+    float NDF = DistributionGGX(normal, halfVec, roughness);
+    float G = GeometrySmith(normal, viewDir, lightDir, roughness);
+    vec3 F = fresnelSchlick(max(dot(halfVec, viewDir), 0.0), F0);
+
+    vec3 kS = F;
+    vec3 kD = vec3(1.0) - kS;
+    kD *= 1.0 - metallic;
+
+    vec3 numerator = NDF * G * F;
+    float denominator = 4.0 * max(dot(normal, viewDir), 0.0) * max(dot(normal, lightDir), 0.0) + 0.0001;
+    vec3 specular = numerator / denominator;
+    float NdotL = max(dot(normal, lightDir), 0.0);
+
+    return (kD * albedo / PI + specular) * radiance * NdotL;
 }
 void main() {
+    //Albedos are done in sRGB color space
+    vec3 albedo = pow(texture(texture_diffuse1, TexCoords).rgb, vec3(2.2));
     vec3 normal = Normal;
-    normal = texture(uNormalTexture, TexCoords).rgb;
+    normal = texture(texture_normal1, TexCoords).rgb;
     normal = normalize(normal * 2.0 - 1.0);
-    vec3 diffuseColor = texture(uDiffuseTexture, TexCoords).rgb;
-    //float specularStrength = texture(uSpecularTexture, TexCoords).r;
+    float metallic = texture(texture_metallic_roughness1, TexCoords).b;
+    float roughness = texture(texture_metallic_roughness1, TexCoords).g;
 
-    vec3 ambient = uAmbientStrength * diffuseColor;
-
-    //vec3 lightDir = normalize(uLightPos - FragPos);
-/*vec3 lightDir = normalize(TangentLightPos - TangentFragPos);
-    float diff = max(dot(normal, lightDir), 0.0);
-    vec3 diffuse = uLightColor * (diff * diffuseColor);*/
-
-    //vec3 viewDir = normalize(uViewPos - FragPos);
+    vec3 F0 = mix(vec3(0.04), albedo, metallic);
+    vec3 Lo = vec3(0.0);
+    vec3 ambient = uAmbientStrength * albedo;
     vec3 viewDir = normalize(TangentViewPos - TangentFragPos);
-    //vec3 reflectDir = reflect(-lightDir, normal);
-    //float spec = pow(max(dot(viewDir, reflectDir), 0.0), uShininess);
-    //vec3 specular = uLightColor * (spec * specularStrength);
-    vec3 result = ambient;
-    result += CalcDirLight(uDirLight, normal, viewDir, diffuseColor);
+    Lo += CalcDirLight(uDirLight, normal, viewDir, albedo, metallic, roughness, F0);
     for (int i = 0; i < NUM_LIGHTS - 1; i++) {
-        result += CalcPointLight(uPointLights[i], TangentLightPos[i], normal, TangentFragPos, viewDir, diffuseColor);
+        Lo += CalcPointLight(uPointLights[i], TangentLightPos[i], TangentFragPos, normal, viewDir, albedo, metallic, roughness, F0);
     }
     vec3 emission = vec3(0.0);
-/*if (textureSize(uEmissiveTexture, 0).x > 0) {
-        emission = texture(uEmissiveTexture, TexCoords).rgb;
-    }*/
-    FragColor = vec4(result, 1.0);
+    //emission = texture(texture_emissive1, TexCoords).rgb;
+    vec3 color = Lo + ambient + emission;
+    color = color / (color + vec3(1.0));
+    color = pow(color, vec3(1.0 / 2.2));
+
+
+    FragColor = vec4(color, 1.0);
 }
